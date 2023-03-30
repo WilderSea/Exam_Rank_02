a98917c2ca0ac5b5cebf0b1f14563b7f171e981f
Wilder Sea <msintas-@c3r10s5.42malaga.com>
2023-03-30 14:29:54 +0200

    split

--- Level_4/ft_split.c
+++ Level_4/ft_split.c
@@ -1,38 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-
-// function to create every word
 char *ft_create_word(char *str)
 {
     char *word;
     int len;
     int i;
-    // count the length of each word to reserve malloc
+    
     i = 0;
     len = 0;
-    while(str[i] && str[i] != 32) // primera palabra
+    while(str[i] && str[i] != 32)
     {
         i++;
         len++;
     }
-    //printf("len first word: %d\n", len);
-    //printf("str: %s\n", str);
     word = malloc(sizeof(char) * (len + 1));
     if (!word)
     {
         return (NULL);
     }
-    // copiar cada letra para formar la palabra
     i = 0;
     while (i < len)
     {
         word[i] = str[i];
-        //printf("str[i]: %c\n", str[i]);
         i++;
     }
     word[i] = '\0';
-    printf("the word is: %s\n", word);
     return (word);
 }
 
@@ -44,21 +37,14 @@ int ft_count_words(char *str)
 
     words = 0;
     i = 0;
-    printf("%s\n", str);
-    
     while(str[i])
     {
-        // la primera palabra hay que contarla
-        //if (i == 0 && (str[i] == 32 || str[0] == '\t'))
         if (i == 0 && str[i] != 32)
         {
             words++;
         }
-        
-        //if (str[i] == 32 || str[i] == '\t') 
         if (str[i] == 32) 
         {
-            //while (str[i] == 32 || str[i] == '\t')
             while (str[i] == 32)
             {
                 i++;
@@ -68,10 +54,9 @@ int ft_count_words(char *str)
         i++;
     }
     return (words);
-
 }
 
-void ft_print_array(char **array)
+/*void ft_print_array(char **array)
 {
     int i;
 
@@ -81,7 +66,7 @@ void ft_print_array(char **array)
         printf("%s\n", array[i]);
         i++;
     }
-}
+}*/
 
 char **ft_split(char *str)
 {
@@ -92,18 +77,11 @@ char **ft_split(char *str)
 
     w = 0;
     num_words = ft_count_words(str);
-    // count words to do malloc
     array = malloc(sizeof(char *) * (num_words + 1));
-    
     if (!array)
     {
         return (NULL);
     }
-    printf("num_words is: %d\n", num_words);
-    // crear las palabras
-    // mientras no lleguemos al total de palabras, las vamos creando
-    printf("str: %s\n", str);
-    // antes tener en cuenta que la frase puede empezar con espacios
     while(*str && *str == 32)
     {
         str++;
@@ -112,27 +90,16 @@ char **ft_split(char *str)
     {
         array[w] = ft_create_word(str);
         w++;
-        // aqui hay que avanzar el puntero para ir a la siguiente palabra
-        // primero avanzamos sobre las letras para llegar al espacio
         while (*str && *str!= 32)
         {
             str++;
         }
-        // y luego avanzamos sobre los espacios
         while (*str && *str == 32)
         {
             str++;
         }
-        // y ya arriba, el str estará colocado al principio de la siguiente palabra, para enviarselo a create word
     }
     array[w] = "\0";
-    ft_print_array(array);
+    //ft_print_array(array);
     return (array);
-}
-
-
-int main(void)
-{
-    ft_split("  hay     que estudiar    y ya está");
-    return (0);
 }
