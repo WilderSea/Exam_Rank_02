548d61ee5c9f081fa87540905a67bdf4209daf73
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-05 12:40:14 +0200

    Update epur_str.c
    delete unused ft_strlen

--- epur_str.c
+++ epur_str.c
@@ -1,17 +1,5 @@
 #include <unistd.h>
 
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while(str[i])
-    {
-        i++;
-    }
-    return (i);
-}
-
 void ft_epur_str(char *str)
 {
     int i;
