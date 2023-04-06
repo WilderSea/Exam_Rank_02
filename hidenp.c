a93653702080cf57ca6a32a66f8312fb2ac800b2
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-06 23:39:04 +0200

    Update hidenp.c

--- hidenp.c
+++ hidenp.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 
 int ft_strlen(char *str)
@@ -15,9 +14,6 @@ int ft_strlen(char *str)
 
 void ft_hidenp(char *s1, char *s2)
 {
-    // strchr
-    // find a char in string
-    // cada vez que encuentre el char en el string, avanza al siguiente char
     int i;
     int j;
     int found;
@@ -26,21 +22,15 @@ void ft_hidenp(char *s1, char *s2)
     i = 0;
     j = 0;
     found = 0;
-    // contar caracteres del s1 para despues saber si los encontró todos
     len = ft_strlen(s1);
     while(s1[i])
     {
-        //printf("%c\n", s1[i]);
         while(s2[j])
         {
             if (s1[i] == s2[j])
             {
-                //printf("found %c\n", c);
                 found++;
                 break;
-                // este break sale del while
-                // y se viene al i++
-                // y empiezo a buscar la siguiente letra del s1
             }
             j++;
         }
