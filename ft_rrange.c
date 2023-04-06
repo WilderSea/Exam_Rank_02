07a42dad34cde31b5abb46f4c9cb960ef1217200
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-06 20:47:30 +0200

    Update ft_rrange.c

--- ft_rrange.c
+++ ft_rrange.c
@@ -1,31 +1,14 @@
 #include <stdlib.h>
-#include <stdio.h>
-
-void ft_print_array(int *array, int len)
-{
-    int i;
-    
-    i = 0;
-    while(i < len)
-    {
-        printf("%d\n", array[i]);
-        i++;
-    }
-}
 
 int *ft_rrange(int start, int end)
 {
     int *array;
     int i;
-    // saber cuantos numeros hay entre el start y el end
     int size;
 
     i = 0;
     size = abs(end - start) + 1;
-    printf("size: %d\n", size);
-    // reservar sitio para un nuevo array con malloc con ese tamaño
     array = malloc(sizeof(int) * size);
-    // rellenar el array con esos valores pero empezando por el end
     while(i < size)
     {
         array[i] = end;
@@ -39,14 +22,5 @@ int *ft_rrange(int start, int end)
         }
         i++;
     }
-    ft_print_array(array, size);
-
-
     return (array);
-}
-
-int main(void)
-{   
-    ft_rrange(1, 3);
-    return (0);
 }
