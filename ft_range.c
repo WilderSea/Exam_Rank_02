6859a8191761341ef674ccd09e5836586861957b
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-06 18:54:20 +0200

    Update ft_range.c
    Preserve only the function code

--- ft_range.c
+++ ft_range.c
@@ -1,17 +1,4 @@
 #include <stdlib.h>
-#include <stdio.h>
-
-void ft_print_array(int *array, int len)
-{
-    int i;
-
-    i = 0;
-    while(i < len)
-    {
-        printf("elemento %d: %d\n", i, array[i]);
-        i++;
-    }
-}
 
 int *ft_range(int start, int end)
 {
@@ -20,15 +7,8 @@ int *ft_range(int start, int end)
     int i;
 
     i = 0;
-    // calcular cuantos elementos va a tener el array considerando start y end
     size = abs(end - start) + 1;
-    // abs = "Absolute Value"
-    // absolute value of a number is that number without its sign   
-    // abs in c: is the distance between that number and 0 without taking direction into account
-    printf("hay %d elementos\n", size);
-    // reservar memoria con ese tamaño
     array = (int *)malloc(sizeof(int) * size);
-    // rellenar el array con esos valores
     while(i < size)
     {
         array[i]= start;
@@ -42,15 +22,5 @@ int *ft_range(int start, int end)
         }
         i++;
     }
-    ft_print_array(array, size);
-    // devolver el puntero al primer elemento del array
     return (array);
-}
-
-
-int main(void)
-{
-    ft_range(0 , -3);
-    
-    return (0);
 }
