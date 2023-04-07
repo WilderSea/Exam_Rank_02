8c97469bd26b06b64d06de09a5e1e69400dea90c
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-07 14:27:31 +0200

    Update lcm.c

--- lcm.c
+++ lcm.c
@@ -1,22 +1,14 @@
-#include <stdio.h>
-
-
-// the least common multiple of two or more numbers, is the smallest number (not counting 0)
-// which is multiple of all these numbers
-
-// para encontrar el multiplo que coincida
-// hacer un bucle que no pare hasta que encuentre 1 multiplo en comun
+/* the least common multiple of two or more numbers, is the smallest number (not counting 0)
+which is multiple of all these numbers */
 
 unsigned int lcm(unsigned int a, unsigned int b)
 {
-    int largest; // hay que saber cual de los dos valores es el mayor
+    int largest;
     
-    // si cualquiera de los dos valores es 0, no se puede hacer el ejercicio
     if (a == 0 || b == 0)
     {
         return (0);
     }
-    // guardar el valor del mayor. Se usará en el bucle para dividir.
     if (a > b)
     {
         largest = a;
@@ -25,9 +17,6 @@ unsigned int lcm(unsigned int a, unsigned int b)
     {
         largest = b;
     }
-    // bucle infinito hasta encontrar el primer multiplo comun
-    // while(1) es bucle infinito.
-    // cuando largest dividido por cada uno de los numeros a y b, tenga resto 0, será el comun multiplo
     while(1)
     {
         if (largest % a == 0 && largest % b == 0)
@@ -39,10 +28,4 @@ unsigned int lcm(unsigned int a, unsigned int b)
             largest++;
         }
     }
-}
-
-int main(void)
-{
-    printf("lcm: %d\n", lcm(8, 12));
-    return (0);
 }
