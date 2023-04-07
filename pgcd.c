74f7f4b751954260d265b5c0ff2ae74b4909aed4
msintas <59841444+WilderSea@users.noreply.github.com>
2023-04-07 20:31:10 +0200

    Create pgcd.c

---
+++ pgcd.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+MAXIMO COMUN DIVISOR
+Greatest Common Denominator
+
+MCD(a, b) = a * b / MCM(a, b)
+
+GCD(a, b) = a * b / LCM(a, b)
+
+*/
+
+int ft_lcm(int a, int b)
+{
+    int largest;
+
+    // si a y b son 0, no se puede hacer el ejercicio
+    if (a == 0 || b == 0)
+    {
+        return (0);
+    }
+    // comprobar cual es mayor, a ó b
+    if (a > b)
+    {
+        largest = a;
+    }
+    else
+    {
+        largest = b;
+    }
+    // teniendo en cuenta el mayor, calcular a partir de ahi cual es el primer multiplo comun que encontramos
+    // bucle infinito while(1) para aumentar largest hasta encontrar el comun multiplo
+    while(1)
+    {
+        if (largest % a == 0 && largest % b == 0)
+        {
+            return (largest);
+        }
+        largest++;
+    }
+}
+
+/*
+  máximo común divisor = factores comunes con menor exponente
+  
+  36 => (2*2) * (3*3)
+  60 => (2*2) * 3 * 5
+  
+  MCD => (2*2) * 3 = 12
+  Máximo Común Divisor (MCD) = Highest/Greatest Common Divisor (GCD)
+  
+  MCD(a,b) = (a * b) / LCM(a,b)
+  
+*/
+
+int ft_pgcd(char *arg_1, char *arg_2)
+{
+    int gcd;
+    int lcm;
+    int a;
+    int b;
+
+    a = atoi(arg_1);
+    b = atoi(arg_2);
+    lcm = ft_lcm(a, b);
+    gcd = (a * b) / lcm;
+    printf("%d\n", gcd);
+    return (gcd);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 3)
+    {
+        ft_pgcd(argv[1], argv[2]);
+    }
+    else
+    {
+        printf("\n");
+    }
+    return (0);
+}
